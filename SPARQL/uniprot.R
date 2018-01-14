125732fac232d0025d250efad4835e5c26a43bf9
Zebulun Arendsee <arendsee@iastate.edu>
2018-01-13 18:22:47 -0600

    Expand SPARQL examples to more fields

--- SPARQL/uniprot.R
+++ SPARQL/uniprot.R
@@ -31,5 +31,22 @@ WHERE
 LIMIT 10
 "
 
+query <-
+"
+PREFIX xsd:<http://www.w3.org/2001/XMLSchema#> 
+PREFIX up:<http://purl.uniprot.org/core/> 
+PREFIX taxon:<http://purl.uniprot.org/taxonomy/> 
+PREFIX faldo:<http://biohackathon.org/resource/faldo#>
+SELECT ?protein ?date ?enzyme ?domain
+WHERE
+{
+	?protein a up:Protein . 
+	?protein up:organism taxon:3702 .
+	?protein up:created ?date .
+	?protein up:enzyme ?enzyme .
+  ?protein up:annotation ?domain .
+  ?domain a up:Domain_Annotation .
+}
+"
 # Step 2 - Use SPARQL package to submit query and save results to a data frame
 d <- SPARQL(url=uptax, query=query)
